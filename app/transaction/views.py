979926e7cb5701b83127bb5393a5c65c46487be5
jmacalawa <jmacalawa@futurex.ph>
2022-05-13 11:11:23 +0800

    done

--- app/transaction/views.py
+++ app/transaction/views.py
@@ -61,6 +61,7 @@ class TransactionBulkCheckout(generics.GenericAPIView):
             x['barangay'] = res.get('barangay')
             x['address'] = res.get('address')
             x['fullname'] = res.get('fullname')
+            x['users_profile'] = res.get('users_profile')
             x['city'] = res.get('city')
             x['zip'] = res.get('zip')
             x['province'] = res.get('province')
